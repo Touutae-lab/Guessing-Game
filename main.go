6871b9f5bce0b9d05dc9b4edc639a7fcd49d7211
Touutae-lab <Pantakan.totae@gmail.com>
2022-11-29 17:56:33 +0700

    update route

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -12,22 +14,30 @@ import (
 // I implement middleware by simple function call
 // Documentation version of middleware commended here
 // ##################################################
-// func auth(next *http.HandlerFunc) http.HandlerFunc {
-// 	return func(res http.ReponseWriter, req *http.Request) {
-// 		token := req.Header.Get("token")
-// 		if token == "token" {
-// 			next(res, req)
-// 		}
-// 		else {
-// 			return res.Write()
-// 		}
-// 	}
-// }
+//
+//	func auth(next *http.HandlerFunc) http.HandlerFunc {
+//		return func(res http.ReponseWriter, req *http.Request) {
+//			token := req.Header.Get("token")
+//			if token == "token" {
+//				next(res, req)
+//			}
+//			else {
+//				return res.Write()
+//			}
+//		}
+//	}
+type Password struct {
+	value string `json:"id"`
+}
 
 // Login
+
+var x int = rand.Int()
+var current_password = new(Password)
+
 func login(res http.ResponseWriter, req *http.Request) {
 	result := make(map[string]string)
-	if req.PostFormValue("token") == "token" {
+	if req.PostFormValue("password") == current_password.value {
 		createJson(res, req)
 	} else {
 		res.WriteHeader(http.StatusNotAcceptable)
@@ -41,8 +51,23 @@ func login(res http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func guessing(res http.ResponseWriter, req *http.Request) {
+func getValue(res http.ResponseWriter, req *http.Request) {
+	res.WriteHeader(http.StatusAccepted)
+	res.Header().Set("Content-Type", "application/json")
 
+	result := make(map[string]string)
+	result["value"] = strconv.Itoa(x)
+
+	data, err := json.Marshal(result)
+	if err != nil {
+		log.Println("Failed to marshaling json")
+		log.Println(err)
+	}
+	res.Write(data)
+}
+
+func guessing(res http.ResponseWriter, req *http.Request) {
+	return
 }
 
 // Create Content for token session
@@ -63,10 +88,33 @@ func createJson(res http.ResponseWriter, req *http.Request) {
 	res.Write(data)
 }
 
+// 2nd Middleware
+func auth(next http.HandlerFunc) http.HandlerFunc {
+	return func(res http.ResponseWriter, req *http.Request) {
+		if req.PostFormValue("token") == "token" {
+			next(res, req)
+		} else {
+			var result = make(map[string]string)
+			res.WriteHeader(http.StatusNotAcceptable)
+			result["message"] = "Not Authorized"
+			data, err := json.Marshal(result)
+			if err != nil {
+				log.Println("Failed to marshaling json")
+				log.Println(err)
+			}
+			res.Write(data)
+		}
+	}
+}
+
 func main() {
 	fmt.Printf("Starting Server at Port 8000\n")
+	current_password.value = "123456"
+	fmt.Printf(current_password.value)
 	router := mux.NewRouter()
-	// router.HandleFunc("/", login).Methods("GET")
 	router.HandleFunc("/login", login).Methods("POST")
+	router.HandleFunc("/guessing", auth(guessing)).Methods("POST")
+	router.HandleFunc("/value", auth(getValue)).Methods("GET")
+	router.HandleFunc("/getPassword")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
